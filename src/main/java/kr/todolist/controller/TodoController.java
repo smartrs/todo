34cac37b304bd53ee96177139d84df138d8c3f1c
edgar <sp.hong@memebox.com>
2018-10-22 09:36:45 +0900

    Optimize Imports

--- src/main/java/kr/todolist/controller/TodoController.java
+++ src/main/java/kr/todolist/controller/TodoController.java
@@ -4,7 +4,6 @@ import kr.todolist.entity.Todo;
 import kr.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
